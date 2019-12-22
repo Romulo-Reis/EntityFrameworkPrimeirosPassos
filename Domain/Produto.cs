d96f7c4a10824b81c6b418b8ed9bd1725d694b3f
Romulo-Reis <romuloreistavares@hotmail.com>
2019-12-22 17:57:20 -0300

    Adição das anotações nas classes do projeto.

--- Domain/Produto.cs
+++ Domain/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,15 @@ namespace Domain
     [Table("Produto")]
     public class Produto
     {
+        [Key]
         public int Id { get; set; }
+        [MaxLength(200)]
         public string Nome { get; set; }
+        [Required]
+        [MaxLength(2000)]
         public string Descricao { get; set; }
+        [Required]
+        [Range(-999999999999.99, 999999999999.99)]
         public Decimal Valor { get; set; }
 
     }
